6fa692ae288239f5a1d2e8e9efca3eb139ae5d71
PandaPandastico <jmsg1984@gmail.com>
2013-01-19 09:12:38 +0000

    Fix it the if condition of the 24-11-2011-1ªParte.

--- 23-11-2011-1ªParte.bash
+++ 23-11-2011-1ªParte.bash
@@ -1,2 +1,3 @@
+#!/bin/bash
 users=(awk '{FS=":"}{499 > $3}{print $1":"$5":"$3}' /etc/passwd)
 
--- 24-11-2011-1ªParte.bash
+++ 24-11-2011-1ªParte.bash
@@ -1,11 +1,13 @@
-if [[ "$#" < 0 ]]; then
+#!/bin/bash
+if [[ "$#" < 1 ]]; then
 	echo "No ha pasado un nombre de usuario válido"
 else
-	GUI=`awk '{FS=":"} $1 == "pandastico" {print $4}' /etc/passwd`
-	adduser --ingroup "$GUI $1.backup"
- 
-	adduser 	if [[ cat /etc/group | awk '{FS=":"}{print $1}' | grep -c backup < 1 ]]; then
+	GUI=`awk '{FS=":"} $1 == "$1" {print $4}' /etc/passwd`
+	adduser --ingroup GUI $1.backup
+	adduser $1 backup
+	BACKGROUP=cat /etc/group | awk '{FS=":"}{print $1}' | grep -c backup
+ 	if [[ $BACKGROUP < 1 ]]; then
 		addgroup backup
 	fi
-	adduser "$1 backup"
+	adduser $1 backup
 fi
