fdbb42d3e4b4d4937b6f97db9af345e460578f70
PandaPandastico <jmsg1984@gmail.com>
2013-01-20 01:40:52 +0000

    Star to refactorice the 24-11-2011-1ªParte

--- 24-11-2011-1ªParte.bash
+++ 24-11-2011-1ªParte.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
 if [[ "$#" < 1 ]]; then
-	echo "No ha pasado un nombre de usuario válido"
-else
-	GUI=`awk '{FS=":"} $1 == "$1" {print $4}' /etc/passwd`
+	aux=(grep "^users:" /etc/group | awk '{FS=":"}{print $4}'
+	
+
+while [[ "$#" > "0" ]]
+do
+	GUI=`awk '{FS=":"} $ == "$1" {print $4}' /etc/passwd`
 	adduser --ingroup GUI $1.backup
-	adduser $1 backup
 	BACKGROUP=cat /etc/group | awk '{FS=":"}{print $1}' | grep -c backup
- 	if [[ $BACKGROUP < 1 ]]; then
+	if [[ $BACKGROUP < 1 ]]; then
 		addgroup backup
 	fi
 	adduser $1 backup
+done
 fi
