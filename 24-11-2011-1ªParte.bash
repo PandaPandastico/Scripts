6491d37a8d3202f48b052984bcb06c480bc16fa3
PandaPandastico <jmsg1984@gmail.com>
2013-01-20 20:48:44 +0000

    Continue the refactorize of 24-11-2011-1ªParte.bash

--- 24-11-2011-1ªParte.bash
+++ 24-11-2011-1ªParte.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
 if [[ "$#" < 1 ]]; then
-	aux=(grep "^users:" /etc/group | awk '{FS=":"}{print $4}'
-	
+	aux=(`grep "^users:" /etc/group | awk 'BEGIN {FS=":"}{print $4}'`)
+	set --$aux
+	IFS=','
+fi
+users=($@)
+unset IFS
 
-while [[ "$#" > "0" ]]
+for index in "${!user[@]}"	
 do
-	GUI=`awk '{FS=":"} $ == "$1" {print $4}' /etc/passwd`
+	GUI=`awk "BEGIN {FS=":"} $1 == ${users[$index]}{print \$4}" /etc/passwd`
 	adduser --ingroup GUI $1.backup
 	BACKGROUP=cat /etc/group | awk '{FS=":"}{print $1}' | grep -c backup
 	if [[ $BACKGROUP < 1 ]]; then
