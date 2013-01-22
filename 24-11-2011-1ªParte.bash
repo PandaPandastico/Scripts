83581129fb930f7f46f6a832f6a11c2e1c9bcace
PandaPandastico <jmsg1984@gmail.com>
2013-01-22 01:16:41 +0000

    Finish the Script 24-11-2011-1ªParte with the all features

--- 24-11-2011-1ªParte.bash
+++ 24-11-2011-1ªParte.bash
@@ -1,20 +1,21 @@
 #!/bin/bash
 if [[ "$#" < 1 ]]; then
-	aux=(`grep "^users:" /etc/group | awk 'BEGIN {FS=":"}{print $4}'`)
-	set --$aux
 	IFS=','
+	aux=(`grep "^users:" /etc/group | awk 'BEGIN {FS=":"}{print $4}'`)
+	set -- $aux
 fi
 users=($@)
 unset IFS
-
-for index in "${!user[@]}"	
+for index in "${!users[@]}"	
 do
-	GUI=`awk "BEGIN {FS=":"} $1 == ${users[$index]}{print \$4}" /etc/passwd`
-	adduser --ingroup GUI $1.backup
-	BACKGROUP=cat /etc/group | awk '{FS=":"}{print $1}' | grep -c backup
-	if [[ $BACKGROUP < 1 ]]; then
-		addgroup backup
-	fi
-	adduser $1 backup
+	gui=`awk -v user=${users[index]} 'BEGIN {FS=":"} $1==user{print $4}' /etc/passwd`
+	name=`awk -v user=${users[index]} 'BEGIN {FS=":"} $1==user{print $5}' /etc/passwd`
+	primarygroup=`awk -v gid=$gui 'BEGIN {FS=":"} $3==gid{print $1}' /etc/group`
+	username=${users[index]}".backup"
+	adduser --ingroup $primarygroup --disabled-password --gecos $name --force-badname $username 
+	addgroup backup
+	adduser $username backup
+	cp -r /home/${users[index]}/* /home/$username
+	chown -R $username /home/$username
+	usermod -L ${users[index]}
 done
-fi
